3d9724ed6eadc60948f1cdec2fee7fd31699f0c2
S16206117 <1458011800@qq.com>
2020-04-22 17:16:26 +0800

    Update fp_brisk.py

--- fp_brisk.py
+++ fp_brisk.py
@@ -29,50 +29,50 @@ kp2, des2 = brisk.detectAndCompute(img2,None)
 img1_kp = cv2.drawKeypoints(img1, kp1, None, (0,255,0), 0)
 img2_kp = cv2.drawKeypoints(img2, kp2, None, (0,255,0), 0)
 
-# BFMatcher with default params
-bf = cv2.BFMatcher()
-matches = bf.knnMatch(des1,des2, k=2)
+# # BFMatcher with default params
+# bf = cv2.BFMatcher()
+# matches = bf.knnMatch(des1,des2, k=2)
+#
+# # Apply ratio test
+# good = []
+# for m,n in matches:
+#     if m.distance < 0.75*n.distance:
+#         good.append([m])
+#
+# # cv2.drawMatchesKnn expects list of lists as matches.
+# img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=2)
 
-# Apply ratio test
-good = []
-for m,n in matches:
-    if m.distance < 0.75*n.distance:
-        good.append([m])
 
-# cv2.drawMatchesKnn expects list of lists as matches.
-img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=2)
+# FLANN parameters
+FLANN_INDEX_KDTREE = 0
+# index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+# while using orb, the params is different with other
+FLANN_INDEX_LSH = 6
+index_params= dict(algorithm = FLANN_INDEX_LSH,
+                   table_number = 6, # 12
+                   key_size = 12,     # 20
+                   multi_probe_level = 1) #2
 
+search_params = dict(checks=50)   # or pass empty dictionary
 
-# # FLANN parameters
-# FLANN_INDEX_KDTREE = 0
-# # index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-# # while using orb, the params is different with other
-# FLANN_INDEX_LSH = 6
-# index_params= dict(algorithm = FLANN_INDEX_LSH,
-#                    table_number = 6, # 12
-#                    key_size = 12,     # 20
-#                    multi_probe_level = 1) #2
-#
-# search_params = dict(checks=50)   # or pass empty dictionary
-#
-# flann = cv2.FlannBasedMatcher(index_params,search_params)
-#
-# matches = flann.knnMatch(des1,des2,k=2)
-#
-# # Need to draw only good matches, so create a mask
-# matchesMask = [[0,0] for i in range(len(matches))]
-#
-# # ratio test as per Lowe's paper
-# for i,(m,n) in enumerate(matches):
-#     if m.distance < 0.7*n.distance:
-#         matchesMask[i]=[1,0]
-#
-# draw_params = dict(matchColor = (0,255,0),
-#                    singlePointColor = (255,0,0),
-#                    matchesMask = matchesMask,
-#                    flags = 0)
-#
-# img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
+flann = cv2.FlannBasedMatcher(index_params,search_params)
+
+matches = flann.knnMatch(des1,des2,k=2)
+
+# Need to draw only good matches, so create a mask
+matchesMask = [[0,0] for i in range(len(matches))]
+
+# ratio test as per Lowe's paper
+for i,(m,n) in enumerate(matches):
+    if m.distance < 0.7*n.distance:
+        matchesMask[i]=[1,0]
+
+draw_params = dict(matchColor = (0,255,0),
+                   singlePointColor = (255,0,0),
+                   matchesMask = matchesMask,
+                   flags = 0)
+
+img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
 
 
 t = (cv2.getTickCount()-t)/cv2.getTickFrequency()
