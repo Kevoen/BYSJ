b13b9687b8f7ae27150790f6a773b648d08c7243
S16206117 <1458011800@qq.com>
2020-05-22 23:18:44 +0800

    Update Stereo_BM.py

--- Stereo_BM.py
+++ Stereo_BM.py
@@ -9,9 +9,10 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
-
-imgL = cv2.imread('imag/teddy/im2.ppm')
-imgR = cv2.imread('imag/teddy/im6.ppm')
+import time
+tic1=time.time()
+imgL = cv2.imread('imag/teddy/im2.ppm',0)
+imgR = cv2.imread('imag/teddy/im6.ppm',0)
 
 # disparity range tuning
 window_size = 5
@@ -19,27 +20,19 @@ min_disp = -1
 max_disp = 79
 num_disp = max_disp - min_disp
 
-stereo = cv2.StereoSGBM_create(
-    minDisparity=min_disp,
-    numDisparities=num_disp,
-    blockSize=3,
-    uniquenessRatio=15,
-    speckleWindowSize=0,
-    speckleRange=2,
-    disp12MaxDiff=1,
-    P1=8 * 3 * window_size ** 2,
-    P2=32 * 3 * window_size ** 2,
-    # preFilterCap=63,
-    # mode=cv2.STEREO_SGBM_MODE_SGBM_3WAY
+stereo = cv2.StereoBM_create(
+    numDisparities=64,
+    blockSize=11,
 )
-disparity = stereo.compute(imgL, imgR).astype(np.float32)/ 16.0
+
+disparity = stereo.compute(imgL, imgR)
 
 size1, size2 = disparity.shape
 
 B = np.load("B.npz")
 M = B['M1']
 f = M[0,0]
-print(M,f,disparity)
+
 depth=np.ones_like(disparity,dtype=np.uint8)
 for i in range(size1):
     for j in range(size2):
@@ -47,10 +40,12 @@ for i in range(size1):
             depth[i][j]=0
         else:
             depth[i][j]=f*100/disparity[i][j]
-
-plt.subplot(221), plt.imshow(imgL)
-plt.subplot(222), plt.imshow(imgR)
-plt.subplot(223), plt.imshow(disparity)
-plt.subplot(224), plt.imshow(depth)
+print(disparity)
+print(depth)
+# plt.subplot(221), plt.imshow(imgL)
+# plt.subplot(222), plt.imshow(imgR)
+plt.subplot(121), plt.imshow(disparity)
+plt.subplot(122), plt.imshow(depth)
+print('用时:',time.time()-tic1)
 plt.show()
 
